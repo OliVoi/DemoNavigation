a9370870277aaea60d1c854bde6123e84a76e269
fuzitsu <hungpopper97@gmail.com>
2018-10-25 09:15:43 +0700

    Initial commit

---
+++ app/src/main/java/com/viettelpost/remoteconfig/demonavigation/MainActivity.java
@@ -0,0 +1,20 @@
+package com.viettelpost.remoteconfig.demonavigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.viettelpost.remoteconfig.demonavigation.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
